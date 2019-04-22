583c82921d3fda51673d5884117a658c24a92cdc
Matthew Bartl <mattbartl@gmail.com>
2019-04-22 20:48:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,8 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
+      
+      req.write item.price
     end
   end
   
