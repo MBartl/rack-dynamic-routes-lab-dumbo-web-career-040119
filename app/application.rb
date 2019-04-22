af4c767dd5de8d30cddaf487dd9dca60614545fa
Matthew Bartl <mattbartl@gmail.com>
2019-04-22 20:46:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    req = Rack::Response.new
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
