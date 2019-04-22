a9b2366508d67e69ea142465f565a56905e756bc
Matthew Bartl <mattbartl@gmail.com>
2019-04-22 20:48:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|item| item.name == item
+      item = @@items.find{|item| item.name == item_name}
     end
   end
   
