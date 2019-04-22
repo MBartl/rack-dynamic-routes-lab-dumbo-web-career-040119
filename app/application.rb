dc71bbadef99d16ff40cc63475a684413c45add7
Matthew Bartl <mattbartl@gmail.com>
2019-04-22 20:49:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     req = Rack::Response.new(env)
     
     if req.path.match(/items/)
+      
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       
